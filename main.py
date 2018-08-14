b579c6bdaef8012772b2afaf0c4e55449ea1fd99
medisp <medisp@icloud.com>
2018-08-14 16:58:38 -0400

    changed layer structure

--- main.py
+++ main.py
@@ -115,9 +115,9 @@ def layers_modified(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
                                kernel_size = 1, 
                                strides=(1,1), 
                                padding = 'same',
-                               #activation=tf.nn.relu,
+                               activation=tf.nn.relu,
                                kernel_initializer = tf.random_normal_initializer(stddev = l2weights_init_const),
-                               #bias_initializer = tf.zeros_initializer(),
+                               bias_initializer = tf.zeros_initializer(),
                                kernel_regularizer = tf.contrib.layers.l2_regularizer(l2weights_reg_const))
     
     #deconvolution + matching output dimensions of layer 4
@@ -126,9 +126,9 @@ def layers_modified(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
                                          kernel_size = 4, 
                                          strides = (2, 2),
                                          padding ='same',
-                                         #activation=tf.nn.relu,
+                                         activation=tf.nn.relu,
                                          kernel_initializer = tf.random_normal_initializer(stddev = l2weights_init_const),
-                                         #bias_initializer = tf.zeros_initializer(),
+                                         bias_initializer = tf.zeros_initializer(),
                                          kernel_regularizer = tf.contrib.layers.l2_regularizer(l2weights_reg_const))
     
     # 1x1 convolution on layer 4
@@ -143,9 +143,9 @@ def layers_modified(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
                                          kernel_size = 4,
                                          strides = (2,2), 
                                          padding = 'same',
-                                         #activation=tf.nn.relu,
+                                         activation=tf.nn.relu,
                                          kernel_initializer = tf.random_normal_initializer(stddev = l2weights_init_const),
-                                         #bias_initializer = tf.zeros_initializer(),
+                                         bias_initializer = tf.zeros_initializer(),
                                          kernel_regularizer = tf.contrib.layers.l2_regularizer(l2weights_reg_const))
     
     # 1x1 convolution on layer 3
@@ -160,9 +160,9 @@ def layers_modified(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
                                          kernel_size = 16, 
                                          strides = 8, 
                                          padding = 'same',
-                                         #activation=tf.nn.relu,
+                                         activation=tf.nn.relu,
                                          kernel_initializer = tf.random_normal_initializer(stddev = l2weights_init_const),
-                                         #bias_initializer = tf.zeros_initializer(),
+                                         bias_initializer = tf.zeros_initializer(),
                                          kernel_regularizer = tf.contrib.layers.l2_regularizer(l2weights_reg_const))
     
     #tf.Print(conv1x1,[tf.shape(deconv3)[1:]])
